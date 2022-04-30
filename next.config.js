85a35efa374fa963c87b0ebbe3d0943dafa2fbf4
Orgil6m <munkhorgil0602@gmail.com>
2022-04-30 09:35:26 +0800

    babel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
 }
 
 module.exports = nextConfig
 module.exports = {
+  "presets": ["next/babel"],
   i18n: {
     locales: ['mn', 'en', 'cn'],
     defaultLocale: 'mn',
     localeDetection: false,
   },
+  
+  
   trailingSlash: true,
+  
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,43 +5,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    // colors: {
-    //   background: {
-    //     light: 'white',
-    //     dark: '#18191A',
-    //   },
-    //   red: {
-    //     light: '#D64635',
-    //     dark: '#DC6456',
-    //   },
-    //   light_pink : {
-    //     light : 'B880FF',
-    //     dark : '#C598FF',
-    //   },
-    //   pink :{
-    //     light : '#FF739D',
-    //     dark : '#FF87AB',
-    //   },
-    //   blue :{
-    //     light : '#4ABAF9',
-    //     dark : '#76C8F6',
-    //   },
-    //   green :{
-    //     light : '#35D6A6',
-    //     dark : '#B0F8CD',
-    //   },
-    //   yellow :{
-    //     light : '#FFD600',
-    //     dark : '#FFF84F',
-    //   },
-    // },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--gradient-color-stops))',
-      }
-    },
-  },
   plugins: [
     require('tailwind-scrollbar-hide'),
      require('@tailwindcss/aspect-ratio'),
