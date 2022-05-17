85c4f2628cb751a394ac10b559459e02822b4aee
Orgil6m <munkhorgil0602@gmail.com>
2022-05-17 14:38:38 +0800

    update

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
+        <div >
             <div className='w-full h-12 bg-neutral-800 text-gray-300 text-sm flex justify-center items-center'>
-                <p>© 2022 Ozzo LLC</p>
+                <p>© 2022 Ozzo LLC <span className='mx-2'> | </span> <span className='font-light text-xs opacity-80'> version 2.0</span></p>
             </div>
         </div>
     )
