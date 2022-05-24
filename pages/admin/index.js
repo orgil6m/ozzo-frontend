0a38967f531f97a13daeb9f0aab869a5748a1f88
Orgil6m <munkhorgil0602@gmail.com>
2022-05-24 19:04:22 +0800

    update

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useContext, useEffect, useState} from 'react'
 import {DataContext} from "../../store/GlobalState"
 import Head from 'next/head'
