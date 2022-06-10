1711cdb5978a78d00c3dd208e26243c6c17808bc
Orgil6m <munkhorgil0602@gmail.com>
2022-06-11 02:36:30 +0800

    sidebar locale

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,6 +6,7 @@ import OzzoLogo from '../Assets/LOGO_ozzo.png'
 import LocaleDropDown from './LocaleDropDown'
 import { DataContext } from '../store/GlobalState';
 import { NavbarLocale} from '../locales/Navbar';
+import { Buttons } from "../locales/Profile"
 
 const Sidebar = () => {
     const {state, dispatch} = useContext(DataContext)
@@ -13,6 +14,8 @@ const Sidebar = () => {
     const router = useRouter()
     const l = router.locale === 'en' ? '1' : router.locale === 'cn' ?  '2'  : '0'
     const t = NavbarLocale[l]
+    const Button = Buttons[l]
+
     const [showSidebar, setShowSidebar] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
 
@@ -36,7 +39,7 @@ const Sidebar = () => {
                         <svg className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
                         </svg>
-                        Админ
+                        {Button.admin}
                     </a>
                 </Link>
                 <div className='w-full h-px bg-gray-200 ml-2'></div>  
@@ -52,7 +55,7 @@ const Sidebar = () => {
                                 <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                             </svg>
-                            Багш
+                            {Button.teacher}
                         </a>
                     </Link>
                     <div className='w-full h-px bg-gray-200 ml-2'></div>   
@@ -66,7 +69,7 @@ const Sidebar = () => {
                             <svg  className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
                             </svg>
-                            Артист
+                            {Button.artist}
                         </a>
                     </Link>
                     <div className='w-full h-px bg-gray-200 ml-2'></div>   
@@ -81,7 +84,7 @@ const Sidebar = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
-                        Сервис
+                        {Button.service}
                     </a>
                 </Link>
                 <div className='w-full h-px bg-gray-200 ml-2'></div>  
@@ -94,7 +97,7 @@ const Sidebar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
-                        Профайл
+                        {Button.profile}
                     </a>
                 </Link>
                 <div className='w-full h-px bg-gray-200 ml-2'></div>   
@@ -102,7 +105,7 @@ const Sidebar = () => {
                     <svg  className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                     </svg>
-                    Гарах
+                    {Button.logout}
                 </div>
             </div>
         )
